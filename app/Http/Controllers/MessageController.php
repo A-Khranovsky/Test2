fd20919d3d10ab2b182e84ad8886ade645e4fa11
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-02-14 19:30:44 +0200

    Created method MessageController-AddMessage

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    public function AddMessage(Request $request)
+    {
+        Message::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'message' => $request->get('message'),
+            ]);
+    }
 }
