28995bc32ca1494d00632962326b61c49dfaf79f
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-02-14 19:57:19 +0200

    Added method MessageController-index. Modified method MessageController-AddMessage()

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -7,12 +7,19 @@ use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
+    public function index()
+    {
+        $messages = Message::all();
+        return view('messages', $messages);
+    }
+
     public function AddMessage(Request $request)
     {
         Message::create([
             'name' => $request->get('name'),
             'email' => $request->get('email'),
             'message' => $request->get('message'),
-            ]);
+        ]);
+        return redirect()->route('home');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('layout');
-});
+//Route::get('/', function () {
+//    return view('layout');
+//});
 
-Route::post('/sendMessage', [MessageController::class, 'AddMessage'])->name('AddMessage');
+Route::post('/sendMessage', [MessageController::class, 'AddMessage'])->name('addMessage');
+Route::get('/', [MessageController::class, 'index'])->name('home');
