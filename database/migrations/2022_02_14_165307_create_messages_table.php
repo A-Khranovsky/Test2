8b0687cede59a2f53901ecdfa2e0821e4dcb8d0c
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2022-02-14 19:36:11 +0200

    Realized migration for table messages

--- database/migrations/2022_02_14_165307_create_messages_table.php
+++ database/migrations/2022_02_14_165307_create_messages_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->mediumText('message');
             $table->timestamps();
         });
     }
